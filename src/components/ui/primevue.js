44d41c059aa74341f9875efc9e423c047103fa6d
AveryOn <lnkonion2@gmail.com>
2024-10-12 14:50:17 +0300

    automation init primevue components

---
+++ src/components/ui/primevue.js
@@ -0,0 +1,8 @@
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+
+// Импорт и глобальное добавление компонентов PrimeVue в приложение происходит здесь 
+export default [
+    Button,
+    Dialog,
+]
